2a759fd2d594b7a1c5ebb8dc98dccd5b5f529516
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2024-09-30 16:57:56 +0300

    Update Program.cs

--- Class_Group_hw_27_06/Class_Group_hw_27_06/Program.cs
+++ Class_Group_hw_27_06/Class_Group_hw_27_06/Program.cs
@@ -7,16 +7,14 @@ namespace Class_Group_hw_27_06
     {
         static void Main(string[] args)
         {
-            //создаём студентов
             Student student1 = new Student("Student", "Studenkov", "Studenkovich", DateTime.Now, new List<int> { 8, 7, 9 }, new List<int> { 9, 9, 9 }, new List<int> { 8, 7, 9 });
             Student student2 = new Student("Petya", "Petrovkin", "Vasilievich", DateTime.Now, new List<int> { 6, 5, 6 }, new List<int> { 5, 6, 6 }, new List<int> { 6, 5, 6 });
             Student student3 = new Student("Mikhail ", "Zubenko", "Petrovich", DateTime.Now, new List<int> { 9, 10, 9 }, new List<int> { 10, 9, 9 }, new List<int> { 9, 10, 9 });
 
-            // Создание компараторов
             Student.NameComparatorAZ nameComparatorAZ = new Student.NameComparatorAZ();
             Student.NameComparatorZA nameComparatorZA = new Student.NameComparatorZA();
 
-            // Сравнение по имени (по возрастанию)
+            //сравнение по имени по возрастанию
             List<Student> studentsByNameAZ = new List<Student> { student1, student2, student3 };
             studentsByNameAZ.Sort(nameComparatorAZ);
             Console.WriteLine("Students sorted by name (A-Z):");
@@ -25,7 +23,7 @@ namespace Class_Group_hw_27_06
                 Console.WriteLine(student.GetName());
             }
 
-            // Сравнение по имени (по убыванию)
+            //сравнение по имени по убыванию
             List<Student> studentsByNameZA = new List<Student> { student1, student2, student3 };
             studentsByNameZA.Sort(nameComparatorZA);
             Console.WriteLine("\nStudents sorted by name (Z-A):");
@@ -34,7 +32,7 @@ namespace Class_Group_hw_27_06
                 Console.WriteLine(student.GetName());
             }
 
-            // 2. Тестирование реализации IEnumerable в классе Group
+            //тестирование реализации IEnumerable в классе Group
             Group group = new Group("P26", 1, Speciallization.SoftwareDeveloper, new List<Student> { student1, student2, student3 });
 
             Console.WriteLine("\nStudents in the group:");
@@ -42,85 +40,6 @@ namespace Class_Group_hw_27_06
             {
                 Console.WriteLine(student.GetName());
             }
-
-            //Student[] students = { student1, student2, student3 };
-
-            ////сортировка студентов по среднему баллу
-            //Array.Sort(students);
-
-            //Console.WriteLine("Students sorted by grade point average:");
-            //foreach (Student student in students)
-            //{
-            //    Console.WriteLine(student);
-            //}
-
-            ////создаём группу
-            //Group group1 = new Group("P26", 1, Speciallization.SoftwareDeveloper, new List<Student>());
-            ////group1.AddStudent(student1);
-            ////group1.AddStudent(student2);
-
-            //Group group2 = new Group("P27", 2, Speciallization.Designer, new List<Student>());
-            ////group2.AddStudent(student3);
-
-            ////массив групп
-            //Group[] groups = { group1, group2 };
-
-            ////сортировка групп по количеству студентов
-            //Array.Sort(groups);
-
-            ////вывод отсортированных групп
-            //Console.WriteLine("\nGroups sorted by number of students:");
-            //foreach (var group in groups)
-            //{
-            //    GroupePrinter.Print(group);
-            //}
-
-            ////Клонирование группы
-            //Group clonedGroup = (Group)group1.Clone();
-            //Console.WriteLine("\nCloned group:");
-            //GroupePrinter.Print(clonedGroup);
-
-            //Console.WriteLine("Student information:");
-            //StudentPrinter.Print(student1);
-            //StudentPrinter.Print(student2);
-            //StudentPrinter.Print(student3);
-
-            ///////////////////
-
-            //Student alex = new Student("Alex", "Asf", "Toren", new DateTime(1999, 5, 24));
-            //alex.SetExamRates(12, 9, 10, 12);
-            //alex.SetHomeworkRates(12, 12, 10, 8);
-            //alex.SetClassWorkRates(11, 11, 10, 10);
-            //Student viva = new Student("Viva", "Vbn", "Sire", new DateTime(2005, 8, 21));
-            //viva.SetExamRates(8, 9, 10, 10);
-            //viva.SetHomeworkRates(8, 9, 10, 10);
-            //viva.SetClassWorkRates(8, 9, 10, 10);
-            //Student lev = new Student("Lev", "FDf", "Tomato", new DateTime(1980, 7, 12));
-            //lev.SetExamRates(7, 3, 8, 4);
-            //lev.SetHomeworkRates(7, 5, 8, 9);
-            //lev.SetClassWorkRates(7, 5, 8, 9);
-            //Student max = new Student("Max", "Studentf", "Normal", DateTime.Now);
-            //max.SetExamRates(10, 5, 9, 11);
-            //max.SetHomeworkRates(10, 10, 9, 11);
-            //max.SetClassWorkRates(10, 10, 9, 11);
-
-            //Group p = new Group();
-            //p.SetName("P26");
-            //p.SetCourseNumber(2);
-            //p.SetSpeciallization(Group.Speciallization.Programming);
-            //p.AddStudent(alex);
-            //p.AddStudent(max);
-
-            //Group p2 = p.Copy();
-            //GroupePrinter.Print(p2);
-
-
-            ////p.DeleteStudent(alex);
-            //p.TransferStudent(alex, p2);
-            //GroupePrinter.Print(p);
-            //p2.SetName("dgdgdg");
-            //GroupePrinter.Print(p2);
-            ////p.StudentsExpulsion();
         }
     }
 }
